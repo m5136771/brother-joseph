9f3af6a24bd28d044fa471aa1c40e14a6ec482e7
Michael DiPaolo <mad.seventwo@gmail.com>
2023-03-30 00:11:50 -0400

    Add handler for select menu choice

--- commands/menus/selectBoMBook.js
+++ commands/menus/selectBoMBook.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const rawData = fs.readFileSync('data/book-of-mormon.json');
-const bookOfMormonData = JSON.parse(rawData);
+const bomData = JSON.parse(rawData);
+
 const { createPages, createPageButtons, updateEmbedWithPage } = require('../../helpers/pagination');
-const { createChapterSelectMenu } = require('../../components/menus');
+const { createChapterSelectMenu } = require('../../components/menus/chapterSelectMenu');
+const { createVerseSelectMenu } = require('../../components/menus/verseSelectMenu');
 
 
 module.exports = {
@@ -13,9 +15,9 @@ module.exports = {
 		await interaction.deferReply({ ephemeral: true });
 
 		const selectedBookIndex = interaction.values[0].split('_').slice(-1)[0];
-		const bookInfo = bookOfMormonData.books.find(book => book.index === selectedBookIndex);
+		const bookInfo = bomData.books.find(book => book.index === selectedBookIndex);
 
-		const chapterSelectMenuRow  = createChapterSelectMenu(bookInfo);
+		const chapterSelectMenuRow = createChapterSelectMenu(bookInfo);
 
 		const embedInfo = {
 			title: bookInfo.book,
@@ -48,6 +50,26 @@ module.exports = {
 				console.log('Pagination timed out');
 			} else {
 				console.error(error);
+			}
+		});
+
+		interaction.client.on('interactionCreate', async (i) => {
+			if (i.isSelectMenu() && i.customId === 'selectBoMChapter') {
+				await i.deferUpdate();
+
+				const selectedChapter = parseInt(i.values[0].split('_')[1]);
+				const verseSelectMenuRow = createVerseSelectMenu(bookInfo, selectedChapter);
+				const currentComponents = i.message.components;
+
+				// Replace or add the verse select menu row
+				const verseSelectMenuRowIndex = currentComponents.findIndex(component => component.type === 'ACTION_ROW' && component.components[0].customId === 'selectBoMVerse');
+				if (verseSelectMenuRowIndex >= 0) {
+					currentComponents[verseSelectMenuRowIndex] = verseSelectMenuRow;
+				} else {
+					currentComponents.push(verseSelectMenuRow);
+				}
+
+				await i.message.edit({ components: currentComponents });
 			}
 		});
 	},
